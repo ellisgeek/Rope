774f6840739be553e3e1f95ec039dfd185eb8e54
Elliott Saille <elliott@rootwerk.systems>
2016-06-15 01:28:38 -0500

    Limit to testing channel temporarily and add magic exit command to properly close bot.

--- Rope.py
+++ Rope.py
@@ -41,7 +41,8 @@ def signal_handler(sig, frame):
     logger.info('Caught signal', sig)
     if sig == signal.SIGTERM or sig == signal.SIGINT:
         logger.info("Exiting")
-        sys.exit(0)
+        discordClient.close()
+        hangupsClient.close()
 signal.signal(signal.SIGINT, signal_handler)
 signal.signal(signal.SIGTERM, signal_handler)
 
@@ -58,10 +59,15 @@ async def on_message(message):
     logger.debug("Received message from {0} in {1}: {2}".format(message.author.display_name, message.channel, message.content))
 
     # we do not want the bot to reply to itself
-    if message.author == discordClient.user:
+    if message.author == discordClient.user or not message.channel.name == "testing":
         return
+    elif message.content.startswith("!@#exit"):
+        logging.info("Got magic exit sequence, Exiting!")
+        await discordClient.delete_message(message)
+        await discordClient.close()
+
     else:
-        await discordClient.change_nickname(discord.utils.get(message.server.members, name=discordClient.user.name), "[Hangouts] {0}".format(message.author.display_name))
+        await discordClient.change_nickname(discord.utils.get(message.server.members, name=discordClient.user.name), "[Hangouts: {0}] {1}".format("CHAT NAME", message.author.display_name))
         await discordClient.send_message(message.channel, "Received message from {0}: {1}".format(message.author.display_name, message.content))
         await discordClient.change_nickname(discord.utils.get(message.server.members, name=discordClient.user.name), config.DISCORD_NICK)
 
