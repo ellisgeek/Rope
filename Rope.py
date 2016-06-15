68c1002e691dbb36f0c0bac5fd1bc5417a403be3
Elliott Saille <elliott@rootwerk.systems>
2016-06-15 00:50:56 -0500

    Initial work

---
+++ Rope.py
@@ -0,0 +1,68 @@
+"""Rope
+A Discord <-> Hangouts Bridge.
+"""
+
+import asyncio
+import discord.discord as discord
+import hangups
+import logging
+import config
+import signal
+import sys
+
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+logFormatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+
+consoleHandler = logging.StreamHandler()
+consoleHandler.setLevel(logging.DEBUG)
+consoleHandler.setFormatter(logFormatter)
+
+#fileHandler = logging.FileHandler("Rope.log")
+#fileHandler.setLevel(logging.DEBUG)
+#fileHandler.setFormatter(logFormatter)
+
+logger.addHandler(consoleHandler)
+#logger.addHandler(fileHandler)
+
+#initialize discord client
+discordClient = discord.Client()
+
+
+# Obtain hangups authentication cookies, prompting for username and
+# password from standard in if necessary.
+cookies = hangups.auth.get_auth_stdin(config.HANGUPS_TOKEN)
+
+# Instantiate hangups Client instance.
+hangupsClient = hangups.Client(cookies)
+
+def signal_handler(sig, frame):
+    logger.info('Caught signal', sig)
+    if sig == signal.SIGTERM or sig == signal.SIGINT:
+        logger.info("Exiting")
+        sys.exit(0)
+signal.signal(signal.SIGINT, signal_handler)
+signal.signal(signal.SIGTERM, signal_handler)
+
+hangouts = discord.Game(name='Hangouts', url="https://hangouts.google.com", type=1)
+
+@discordClient.event
+async def on_ready():
+    logger.info('Logged in as {0}({1})'.format(discordClient.user.name, discordClient.user.id))
+    logger.info("READY")
+    await discordClient.change_status(game=hangouts)
+
+@discordClient.event
+async def on_message(message):
+    logger.debug("Received message from {0} in {1}: {2}".format(message.author.display_name, message.channel, message.content))
+
+    # we do not want the bot to reply to itself
+    if message.author == discordClient.user:
+        return
+    else:
+        await discordClient.change_nickname(discord.utils.get(message.server.members, name=discordClient.user.name), "[Hangouts] {0}".format(message.author.display_name))
+        await discordClient.send_message(message.channel, "Received message from {0}: {1}".format(message.author.display_name, message.content))
+        await discordClient.change_nickname(discord.utils.get(message.server.members, name=discordClient.user.name), config.DISCORD_NICK)
+
+discordClient.run(config.DISCORD_TOKEN)
